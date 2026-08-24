e02270354b53bf69d12536d21d1b8386a0af7d21
medillofranc <jvjtsasunoni@gmail.com>
2026-08-23 19:20:27 -0700

    Improve CheckWinner(): add win/draw detection and reset

--- Tictactoe/Form1.cs
+++ Tictactoe/Form1.cs
@@ -170,6 +170,33 @@ namespace Tictactoe
 
         private bool CheckWinner()
         {
+            foreach (var combo in winningCombinations)
+            {
+                //Player Win
+                if (combo.All(i => buttons[i - 1].Text == PLAYER))
+                {
+                    MessageBox.Show("Player Wins!");
+                    ClearGame();
+                    return true;
+                }
+
+                //CPU Win
+                if (combo.All(i => buttons[i - 1].Text == CPU))
+                {
+                    MessageBox.Show("CPU Wins!");
+                    ClearGame();
+                    return true;
+                }
+            }
+
+            if (buttons.All(btn => btn.Text != ""))
+            {
+                MessageBox.Show("It's a Draw!");
+                ClearGame();
+                return true;
+            }
+
+
             return false;
         }
 
