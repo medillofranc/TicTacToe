b5f41e6d95a0c3a9f89e12726b3eac5b9478cdfe
medillofranc <jvjtsasunoni@gmail.com>
2026-08-23 18:56:02 -0700

    feat: Add clear game and player actions

---
+++ Tictactoe/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(127, 83);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(96, 96);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "Y";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(229, 83);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(96, 96);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "Y";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(331, 83);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(96, 96);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "Y";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(331, 185);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(96, 96);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "Y";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(229, 185);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(96, 96);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "Y";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(127, 185);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(96, 96);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "Y";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(331, 287);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(96, 96);
+            this.b9.TabIndex = 8;
+            this.b9.Text = "Y";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(229, 287);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(96, 96);
+            this.b8.TabIndex = 7;
+            this.b8.Text = "Y";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(127, 287);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(96, 96);
+            this.b7.TabIndex = 6;
+            this.b7.Text = "Y";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "PLAYER:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "CPU:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 457);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Tictactoe/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tictactoe
+{
+    public partial class Form1 : Form
+    {
+        private Button[] buttons;
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new[] { b1, b2, b3, b4, b5, b6, b7, b8, b9 };
+            foreach (var btn in buttons)
+                btn.Click += PlayerMove;
+
+            ClearGame();
+        }
+
+
+        private void PlayerMove(object sender, EventArgs e)
+        {
+            Button btn = (sender as Button);
+            if (btn is Button)
+            {
+                btn.Text = "X";
+                btn.Enabled = false;
+            }
+        }
+
+
+        private void ClearGame()
+        {
+            foreach (var button in buttons)
+            {
+                button.Text = "";
+                button.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
